93361602c9bec00c3e6cde216c471e733189cc68
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-12-14 20:53:13 -0300

    component App with all the routes and path created

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from './Menu';
+import Navbar from './Navbar';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import SigninPage from '../pages/SigninPage/SiginPage';
+import HabitsPage from "../pages/HabitsPage/HabitsPage";
+import TodayPage from '../pages/TodayPage/TodayPage';
+import HistoryPage from '../pages/HistoryPage/HistoryPage';
+
 
 export default function App() {
   return (
     <div>
-      <p> testando</p>
+      <BrowserRouter>
+        <Navbar />
+        <Menu />
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/cadastro' element={<SigninPage />} />
+          <Route path='/habitos' element={< HabitsPage />} />
+          <Route path='/hoje' element={<TodayPage />} />
+          <Route path='/historico' element={<HistoryPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-export default function Footer() {
+export default function Menu() {
     return (
         <div>
 
